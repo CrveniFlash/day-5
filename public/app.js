28ca8cc779c41f5342c656cd723c4ca25f3214f0
Alen <redflashms@gmail.com>
2021-09-10 19:29:14 -0400

    day5 practise

--- public/app.js
+++ public/app.js
@@ -1,2 +1,9 @@
 const buttons = document.querySelectorAll('.btn')
 const imgDiv = document.querySelector('.img-container')
+
+const images = [
+    'rolls-1.jpg',
+    'rolls-2.jpg',
+    'rolls-3.jpg',
+    'rolls-4.jpg',
+]
