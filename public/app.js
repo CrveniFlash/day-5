f751fe0df0067359634ad79ce9d39ec7aec71f0b
Alen <redflashms@gmail.com>
2021-09-10 19:37:00 -0400

    day5 practise

--- public/app.js
+++ public/app.js
@@ -7,3 +7,14 @@ const images = [
     'rolls-3.jpg',
     'rolls-4.jpg',
 ]
+
+let imageNum = 0;
+
+buttons.forEach(function(button){
+    button.addEventListener('click', (e) => {
+        const item = e.target.classList
+        if(item.contains('next')){
+            imageNum++
+        }
+    })
+})
